ab5dff50f4651abb6105c25246b7202f29cb1b92
evank12 <kurukato2419@gmail.com>
2026-03-06 20:54:24 -0600

    Remove database connection test from server.js
    Removed database connection test code.

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-// Test database connection
-try {
-    db.prepare('SELECT 1').get();
-    console.log('Database connected');
-} catch (err) {
-    console.error('Database error:', err);
-    process.exit(1);
-}
-
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
